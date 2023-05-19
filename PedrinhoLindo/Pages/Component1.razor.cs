5dacd0440bc8ea836f397ac1e9a4cb9ba148853a
WellintonRafael <wellintonrafaelsantos@gmail.com>
2023-05-19 16:52:35 -0300

    first commit

---
+++ PedrinhoLindo/Pages/Component1.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PedrinhoLindo.Pages
+{
+    public partial class Component1
+    {
+        public string Nome { get; set; }
+
+        public string Teste
+        {
+            get => _teste;
+            set
+            {
+                _teste = value;
+
+                InvokeAsync(() => StateHasChanged());
+            }
+        }
+        private string _teste;
+    }
+}
